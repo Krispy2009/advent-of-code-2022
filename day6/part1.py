8cb86d518f6d2e3a4fbf33f45a2ba9d2459e5b26
Kristi Achilleos <Krispy2009@gmail.com>
2022-12-06 14:32:16 +0200

    day 6 part 1

---
+++ day6/part1.py
@@ -0,0 +1,27 @@
+MARK_LENGTH = 4
+
+def read_input(filename):
+    lines = []
+    with open(filename) as f:
+        for line in f.readlines():
+            lines.append(line.strip())
+    return lines
+    
+
+
+def find_mark(line):
+    index = MARK_LENGTH
+    while index < len(line):
+        sub_str =  line[index-MARK_LENGTH:index]
+        if is_mark(sub_str):
+            print(f'mark found at index: {index}')
+            break
+        index +=1
+
+def is_mark(sub_str):
+    return len(set(list(sub_str))) == MARK_LENGTH
+
+if __name__ == "__main__":
+    lines = read_input('input.txt')
+    for line in lines:
+        find_mark(line)
